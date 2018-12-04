1b494b2b6a77ac6fd6e96a7224040638f71cfbe9
prithvi-surana <44387753+prithvi-surana@users.noreply.github.com>
2018-12-04 14:18:19 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,9 @@
+sum=0
+l=[]
+for x in range(0,11,2):
+  print(x)
+  l.append(x)
+  for i in l:
+     sum=sum+i
+print(sum)
+  
---
+++ reverse.py
@@ -0,0 +1,8 @@
+a=str(input())
+def reverse(a):
+   str1=''
+   for i in a:
+      str1=i+str1
+   return str1
+print(reverse(a))
+      
